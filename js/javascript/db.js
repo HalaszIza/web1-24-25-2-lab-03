1359a69c4c9cd42ae688bd456a3055154a1acc70
HalaszIza <halasziza@gmail.com>
2025-03-27 17:20:23 +0100

    lab-06

---
+++ js/javascript/db.js
@@ -0,0 +1,20 @@
+export const userList = [
+    {
+        id: 1,
+        name: 'Priszcilla',
+        age: 28,
+    },
+    {
+        id: 2,
+        name: 'Anastasia',
+        age: 18,
+    },
+    {
+        id: 3,
+        name: 'Little Mermaid',
+        age: 102,
+    },
+];
+// tömb, amiben objektumok vannak
+
+export const active = true;
